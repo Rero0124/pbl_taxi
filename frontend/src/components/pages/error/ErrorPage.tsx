2109449502171a5916071aad933b4341b9eabfa9
Rero0124 <kimseung0930@gmail.com>
2024-04-15 01:36:19 +0900

    style: 문구수정

--- frontend/src/components/pages/error/ErrorPage.tsx
+++ frontend/src/components/pages/error/ErrorPage.tsx
@@ -10,7 +10,7 @@ const Page404 = ({ contentType }: Props): JSX.Element => {
   switch (contentType) {
     case "404": message = `잘못된 페이지입니다.`; break;
     case "500": message = `서버 오류입니다.`; break;
-    default: message = `현재 ${contentType}은/는 지원되지 않습니다.<br /> 다른 브라우저로 접속해주세요`; break;
+    default: message = `현재 ${contentType}은/는 지원되지 않습니다. 다른 브라우저로 접속해주세요`; break;
   }
 
   return (
